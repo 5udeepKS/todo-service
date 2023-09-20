4b3571102704b639322afde01af25fefff6041d9
5udeepKS <sudeepkanchibailu@gmail.com>
2023-09-21 02:27:36 +0530

    Added logic for fetch all Todos

--- src/main/java/com/sudeep/fullstackprojects/todo/controller/TodoController.java
+++ src/main/java/com/sudeep/fullstackprojects/todo/controller/TodoController.java
@@ -1,8 +1,26 @@
 package com.sudeep.fullstackprojects.todo.controller;
 
+import com.sudeep.fullstackprojects.todo.model.Todo;
+import com.sudeep.fullstackprojects.todo.service.TodoService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api/v1.0/tasks")
 @RestController
+@CrossOrigin("*")
 public class TodoController {
 	
+	@Autowired
+	private TodoService todoService;
+	
+	@GetMapping("/todo")
+	public List<Todo> getAllTask() {
+		return this.todoService.getAllTask();
+	}
 }
--- src/main/java/com/sudeep/fullstackprojects/todo/service/TodoService.java
+++ src/main/java/com/sudeep/fullstackprojects/todo/service/TodoService.java
@@ -1,9 +1,22 @@
 package com.sudeep.fullstackprojects.todo.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.sudeep.fullstackprojects.todo.model.Todo;
+import com.sudeep.fullstackprojects.todo.repository.TodoRepository;
 
 @Service
 public class TodoService {
+	
+	@Autowired
+	private TodoRepository todoRepository;
+
+	public List<Todo> getAllTask() {
+		return this.todoRepository.findAll();
+	}
 
 	
 }
